c965b4c8e311d02e15f71920334e0195838e537d
steevened <alvaradosteven315@gmail.com>
2023-07-06 17:46:59 -0500

    style navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import { ModeToggle } from '@/components/ui/theme-toggle';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24"></main>
+    <main className="min-h-screen">
+      <header className="py-4 border-b border-stone-900/20 dark:border-stone-50/10">
+        <div className="container flex items-center justify-between mx-auto">
+          <Link href="/">
+            <h1 className="text-xl font-bold">USERS CRUD</h1>
+          </Link>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <ModeToggle />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </main>
   );
 }
