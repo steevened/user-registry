55472c385ffa1f6d64d21c516c589038d4aed1ec
steevened <alvaradosteven315@gmail.com>
2023-07-06 17:14:48 -0500

    getting started

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    BASE_URL:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/api'
+        : 'https://api.themoviedb.org/3/',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
